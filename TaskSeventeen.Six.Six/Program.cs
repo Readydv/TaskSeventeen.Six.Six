5e562db8bafec7b6b0840e2393d8cf6c94293ff6
Danil <daniil-vn@mail.ru>
2025-01-26 03:47:23 +0500

    Написан свой вариант рефакторинга кода из задания 4 в соответствии с Принципом открытости-закрытости.

--- TaskSeventeen.Six.Six/Program.cs
+++ TaskSeventeen.Six.Six/Program.cs
@@ -6,7 +6,27 @@ namespace TaskSeventeen.Six.Six
     {
         static void Main(string[] args)
         {
+            // Создаем обычный аккаунт
+            Account ordinaryAccount = new Account
+            {
+                Type = "Обычный",
+                Balance = 800 // Пример баланса
+            };
 
+            // Создаем зарплатный аккаунт
+            Account salaryAccount = new Account
+            {
+                Type = "Зарплатный",
+                Balance = 1500 // Пример баланса
+            };
+
+            // Рассчитываем процентные ставки
+            Calculator.CalculateInterest(ordinaryAccount);
+            Calculator.CalculateInterest(salaryAccount);
+
+            // Выводим результаты
+            Console.WriteLine($"Обычный аккаунт: Баланс = {ordinaryAccount.Balance}, Процентная ставка = {ordinaryAccount.Interest}");
+            Console.WriteLine($"Зарплатный аккаунт: Баланс = {salaryAccount.Balance}, Процентная ставка = {salaryAccount.Interest}");
         }
     }
 }
